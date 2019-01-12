042a4ff55071d00db56e6388e1e729b334b71799
AIRahimi <maile@airahimi.no>
2019-01-12 23:08:42 +0100

    EditText for calendar

--- app/src/main/java/no/airahimi/mydaycalculator/MainActivity.java
+++ app/src/main/java/no/airahimi/mydaycalculator/MainActivity.java
@@ -1,12 +1,10 @@
 package no.airahimi.mydaycalculator;
 
-import android.app.DatePickerDialog;
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,10 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String MY_DATE_FORMAT = "dd.MM.yyyy";
     private Calendar calendarNow;
-    private Calendar calenderThen;
-    private TextView tvResult;
-    private EditText etDate;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,43 +23,22 @@ public class MainActivity extends AppCompatActivity {
         calendarNow = Calendar.getInstance();
         String date = new SimpleDateFormat(MY_DATE_FORMAT).format(calendarNow.getTime());
 
-        calenderThen = Calendar.getInstance();
-
         TextView tvDateFrom = findViewById(R.id.tvDateFrom);
         tvDateFrom.setText(getString(R.string.text_from, date));
-
-        etDate = findViewById(R.id.etDate);
-        tvResult = findViewById(R.id.tvResult);
-    }
-
-    DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
-            calenderThen.set(Calendar.YEAR, year);
-            calenderThen.set(Calendar.MONTH, monthOfYear);
-            calenderThen.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
-            String date = new SimpleDateFormat(MY_DATE_FORMAT).format(calenderThen.getTime());
-            etDate.setText(date);
-        }
-    };
-
-    public void setDate(View view) {
-        DatePickerDialog picker = new DatePickerDialog(MainActivity.this, dateListener, calenderThen
-                .get(Calendar.YEAR), calenderThen.get(Calendar.MONTH),
-                calenderThen.get(Calendar.DAY_OF_MONTH));
-        picker.show();
     }
 
     public void calculate(View view) {
+        EditText etDate = findViewById(R.id.etDate);
         String toDate = etDate.getText().toString();
+
+        TextView tvResult = findViewById(R.id.tvResult);
+
         try {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat(MY_DATE_FORMAT);
-            calenderThen = Calendar.getInstance();
-            calenderThen.setTime(simpleDateFormat.parse(toDate));
+            Calendar toCalendar = Calendar.getInstance();
+            toCalendar.setTime(simpleDateFormat.parse(toDate));
 
-            long diff = calendarNow.getTimeInMillis() - calenderThen.getTimeInMillis();
+            long diff = calendarNow.getTimeInMillis() - toCalendar.getTimeInMillis();
             diff = Math.abs(diff);
             long days = diff / 1000 / 60 / 60 / 24;
             diff -= days * 1000 * 60 * 60 * 24;
@@ -75,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             String result = String.format("%dd %dt %dm", days, hours, mins);
             tvResult.setText(result);
         } catch (ParseException ex) {
-            tvResult.setText("Please select a date");
+            tvResult.setText("Date incorrect");
         } finally {
             tvResult.setVisibility(View.VISIBLE);
         }
